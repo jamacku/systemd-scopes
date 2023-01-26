b19aedd4e56e13b3c8cbf62b63fa3a4546679ca8
Jan Macku <jamacku@redhat.com>
2023-01-26 13:57:33 +0100

    src: Add command parameter `--sleep`

--- src/main.py
+++ src/main.py
@@ -8,6 +8,7 @@ https://github.com/facebookincubator/pystemd/blob/fb124d2f1196af0dcb69fa8fc9bb9c
 import random
 import shlex
 import time
+import argparse
 
 from pystemd.dbuslib import DBus
 from pystemd.systemd1 import Manager, Unit
@@ -15,10 +16,7 @@ from pystemd.systemd1 import Manager, Unit
 
 def start_transient_unit(cmd="/bin/sleep 5"):
     a_cmd = [c.encode() for c in shlex.split(cmd)]
-    random_unit_name = "myservice.{r}.{t}.service".format(
-        r=random.randint(0, 100), t=time.time()
-    ).encode()
-
+    random_unit_name = f"myservice.{random.randint(0, 100)}.{time.time()}.service"
     unit = {
         b"Description": b"Example of transient unit",
         b"ExecStart": [(a_cmd[0], a_cmd, False)],
@@ -49,4 +47,20 @@ def start_transient_unit(cmd="/bin/sleep 5"):
 
 
 if __name__ == "__main__":
-    start_transient_unit()
+    parser = argparse.ArgumentParser(
+        description="Create dummy process and assign it to systemd scope. \
+                     Process runs `/bin/sleep <n>`"
+    )
+
+    parser.add_argument(
+        "--sleep",
+        type=int,
+        default=0,
+        nargs=1,
+        help="number of seconds for /bin/sleep",
+    )
+
+    ARGS = parser.parse_args()
+    SLEEP_CMD = f"/bin/sleep {ARGS.sleep[0]}"
+
+    start_transient_unit(SLEEP_CMD)
